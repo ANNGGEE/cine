a082b6922e2c30f087da7311715b3a478ae1c3be
Angela <angelagomezlopez4@gmail.com>
2026-01-26 20:16:15 +0100

    CommitMessage

--- src/main/java/com/example/cine/entity/Butaca.java
+++ src/main/java/com/example/cine/entity/Butaca.java
@@ -1,4 +1,20 @@
 package com.example.cine.entity;
 
-public class Butaca {
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Butaca{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_butaca;
+
+    private String posicion;
+
+    private String fila;
+    private Integer numero;
+
+    @OneToOne(mappedBy = "butaca")
+    private Entrada entrada;
 }
--- src/main/java/com/example/cine/entity/Entrada.java
+++ src/main/java/com/example/cine/entity/Entrada.java
@@ -6,10 +6,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
+import java.util.Date;
+
 @Entity
 @Data
 public class Entrada{
+    private Date fecha_compra;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_proyeccion", referencedColumnName = "id")
     private Proyeccion proyeccion;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_butaca", referencedColumnName = "id")
+    private Butaca butaca;
 }
