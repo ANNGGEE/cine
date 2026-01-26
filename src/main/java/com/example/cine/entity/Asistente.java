5e0b98ea59d0c8e283a134e9c117bfaeffb68205
Ángela Gómez López <angelagomezlopez4@gmail.com>
2026-01-26 13:56:46 +0100

    CommitMessage

--- src/main/java/com/example/cine/entity/Asistente.java
+++ src/main/java/com/example/cine/entity/Asistente.java
@@ -2,5 +2,4 @@ package com.example.cine.entity;
 
 public class Asistente {
     private Long id;
-    private String nombre;
 }
---
+++ src/main/java/com/example/cine/repositories/PeliculaRepository.java
@@ -0,0 +1,4 @@
+package com.example.cine.repositories;
+
+public interface PeliculaRepository {
+}
