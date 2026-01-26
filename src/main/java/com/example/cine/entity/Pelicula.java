2f0c728193cddd4e2efb06114d66d775ca288e49
Angela <angelagomezlopez4@gmail.com>
2026-01-26 20:19:48 +0100

    CommitMessage

--- src/main/java/com/example/cine/entity/Pelicula.java
+++ src/main/java/com/example/cine/entity/Pelicula.java
@@ -1,11 +1,17 @@
 package com.example.cine.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@Entity
 public class Pelicula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_pelicula;
+
     @OneToMany(mappedBy = "pelicula", cascade = CascadeType.ALL)
     private List<Proyeccion> proyeccion;
 }
