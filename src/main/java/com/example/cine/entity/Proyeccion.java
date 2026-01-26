8056356c8862be888be5f89c3f4fdaba581695af
Ángela Gómez López <angelagomezlopez4@gmail.com>
2026-01-26 14:09:33 +0100

    CommitMessage

--- src/main/java/com/example/cine/entity/Proyeccion.java
+++ src/main/java/com/example/cine/entity/Proyeccion.java
@@ -1,4 +1,17 @@
 package com.example.cine.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
 public class Proyeccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_proyeccion;
+
+    @OneToMany(mappedBy = "proyeccion", cascade = CascadeType.ALL)
+    private List<Sala> sala;
 }
--- src/main/java/com/example/cine/entity/Sala.java
+++ src/main/java/com/example/cine/entity/Sala.java
@@ -1,4 +1,19 @@
 package com.example.cine.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Sala {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_sala;
+
+    private String descripcion;
+    private int numButaca;
+
+    @ManyToOne
+    @JoinColumn(name = "proyeccion_id")
+    private Proyeccion proyeccion;
 }
