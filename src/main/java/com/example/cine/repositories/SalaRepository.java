25d79f97eb1a473a597d2a127c08c475e3edfdcf
Angela <angelagomezlopez4@gmail.com>
2026-01-28 23:58:47 +0100

    CommitMessage

--- src/main/java/com/example/cine/repositories/SalaRepository.java
+++ src/main/java/com/example/cine/repositories/SalaRepository.java
@@ -1,4 +1,13 @@
 package com.example.cine.repositories;
 
-public interface SalaRepository {
+import com.example.cine.entity.Proyeccion;
+import com.example.cine.entity.Sala;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SalaRepository extends JpaRepository<Sala, Long> {
+
 }
