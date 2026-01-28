ae9194b947996ea48f914f31569bb04e037188fd
Angela <angelagomezlopez4@gmail.com>
2026-01-28 23:55:16 +0100

    CommitMessage

--- src/main/java/com/example/cine/controllers/SalaController.java
+++ src/main/java/com/example/cine/controllers/SalaController.java
@@ -1,4 +1,36 @@
 package com.example.cine.controllers;
 
+import com.example.cine.entity.Sala;
+import com.example.cine.services.SalaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/salas")
 public class SalaController {
+    private final SalaService salaService;
+
+    public SalaController(SalaService salaService) {
+        this.salaService = salaService;
+    }
+
+    // ================= LISTAR TODAS LAS SALAS ============
+    @GetMapping
+    public ResponseEntity<List<Sala>> listarSalas(){
+        return ResponseEntity.ok(salaService.obtenerTodas());
+    }
+
+    // =============== OBTENER SALA POR ID ===================
+    @GetMapping("/{id}")
+    public ResponseEntity<Sala> obtenerSala(@PathVariable Long id){
+        return ResponseEntity.ok(salaService.obtenerPorId(id));
+    }
+
+    // ================ CREAR SALA ==========================
+    @PostMapping
+    public ResponseEntity<Sala> crearSala(@RequestBody Sala sala){
+        return ResponseEntity.ok(salaService.crearSala(sala));
+    }
 }
--- src/main/java/com/example/cine/services/SalaService.java
+++ src/main/java/com/example/cine/services/SalaService.java
@@ -1,4 +1,32 @@
 package com.example.cine.services;
 
-public interface SalaService {
+import com.example.cine.entity.Sala;
+import com.example.cine.repositories.SalaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalaService {
+    private final SalaRepository salaRepository;
+
+    public SalaService(SalaRepository salaRepository) {
+        this.salaRepository = salaRepository;
+    }
+
+    // ================== OBTENER TODAS LAS SALAS ========================
+    public List<Sala> obtenerTodas(){
+        return salaRepository.findAll();
+    }
+
+    // ============================== OBTENER SALAS POR ID =========================
+    public Sala obtenerPorId(Long id_sala){
+        return salaRepository.findById(id_sala)
+                .orElseThrow(() -> new RuntimeException("Sala no encontrada"));
+    }
+
+    // ================= CREAR SALA ====================
+    public Sala crearSala(Sala sala){
+        return salaRepository.save(sala);
+    }
 }
