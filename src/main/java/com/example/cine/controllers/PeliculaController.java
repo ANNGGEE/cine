0a9f3368c08fbd0ca538dda506b7fccd3315ebd5
Angela <angelagomezlopez4@gmail.com>
2026-01-29 00:45:32 +0100

    CommitMessage

--- src/main/java/com/example/cine/controllers/PeliculaController.java
+++ src/main/java/com/example/cine/controllers/PeliculaController.java
@@ -1,4 +1,49 @@
 package com.example.cine.controllers;
 
+import com.example.cine.entity.Pelicula;
+import com.example.cine.services.PeliculaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/peliculas")
 public class PeliculaController {
+    private final PeliculaService peliculaService;
+
+    public PeliculaController(PeliculaService peliculaService) {
+        this.peliculaService = peliculaService;
+    }
+
+    // ================= LISTAR TODAS LAS PELICULAS ================
+    @GetMapping
+    public ResponseEntity<List<Pelicula>> listarPeliculas() {
+        return ResponseEntity.ok(peliculaService.obtenerTodas());
+    }
+
+    // ================= OBTENER PELICULA POR ID ================
+    @GetMapping("/{id}")
+    public ResponseEntity<Pelicula> obtenerPelicula(@PathVariable Long id){
+        return ResponseEntity.ok(peliculaService.obtenerPorId(id));
+    }
+
+    // ==================== CREAR PELICULA ====================
+    @PostMapping
+    public ResponseEntity<Pelicula> crearPelicula(@RequestBody Pelicula pelicula){
+        return ResponseEntity.ok(peliculaService.crearPelicula(pelicula));
+    }
+
+    // ==================== ACTUALIZAR PELICULA =========================
+    @PutMapping("/{id}")
+    public ResponseEntity<Pelicula> actualizarPelicula(@PathVariable Long id, @RequestBody Pelicula pelicula){
+        return ResponseEntity.ok(peliculaService.actualizarPelicula(id, pelicula));
+    }
+
+    // ========================= ELIMINAR PELICULA ===========================
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarPelicula(@PathVariable Long id){
+        peliculaService.eliminarPelicula(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/cine/services/PeliculaService.java
+++ src/main/java/com/example/cine/services/PeliculaService.java
@@ -1,4 +1,48 @@
 package com.example.cine.services;
 
-public interface PeliculaService {
+import com.example.cine.entity.Pelicula;
+import com.example.cine.repositories.PeliculaRepository;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PeliculaService {
+    private final PeliculaRepository peliculaRepository;
+
+    public PeliculaService(PeliculaRepository peliculaRepository) {
+        this.peliculaRepository = peliculaRepository;
+    }
+
+    // ================ LISTAMOS TODAS LAS PELICULAS ================
+    public List<Pelicula> obtenerTodas(){
+        return peliculaRepository.findAll();
+    }
+
+    // ===================== OBTENER POR ID ======================
+    public Pelicula obtenerPorId(Long id_pelicula){
+        return peliculaRepository.findById(id_pelicula)
+                .orElseThrow(() -> new RuntimeException("Película no encontrada"));
+    }
+
+    // ================= CREAR PELICULA ========================
+    public Pelicula crearPelicula(Pelicula pelicula){
+        return peliculaRepository.save(pelicula);
+    }
+
+    // ===================== ACTUALIZAR PELICULA ===========================
+    public Pelicula actualizarPelicula(Long id, Pelicula peliculaActualizada){
+        Pelicula pelicula = obtenerPorId(id);
+        pelicula.setTitulo(peliculaActualizada.getTitulo());
+        pelicula.setDuracion(peliculaActualizada.getDuracion());
+        pelicula.setGenero(peliculaActualizada.getGenero());
+        return peliculaRepository.save(pelicula);
+    }
+
+    // ==================== ELIMINAR PELICULA ====================
+    public void eliminarPelicula(Long id){
+        Pelicula pelicula = obtenerPorId(id);
+        peliculaRepository.delete(pelicula);
+    }
 }
