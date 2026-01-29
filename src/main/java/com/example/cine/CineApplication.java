17f70255cba2cc6793f5ac6bf34ccfbffba1297f
Angela <angelagomezlopez4@gmail.com>
2026-01-29 02:29:36 +0100

    CommitMessage

--- src/main/java/com/example/cine/CineApplication.java
+++ src/main/java/com/example/cine/CineApplication.java
@@ -1,7 +1,6 @@
 package com.example.cine;
 
 import com.example.cine.services.*;
-import com.example.cine.util.CineConsole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,17 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class CineApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(CineApplication.class, args);
-
-		// Ejecutar la consola
-		CineConsole console = new CineConsole(
-				context.getBean(AsistenteService.class),
-				context.getBean(SalaService.class),
-				context.getBean(PeliculaService.class),
-				context.getBean(ProyeccionService.class),
-				context.getBean(EntradaService.class)
-		);
-
-		console.run();
+		SpringApplication.run(CineApplication.class, args);
 	}
 }
